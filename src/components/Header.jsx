7fab2dd9658e5a61cb905389efaa69637a258e9a
pavankumar <pavankumar.b@ananthacybertech.com>
2024-12-12 19:50:36 +0530

    initial commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,7 +17,7 @@ const Header = ({ onToggleSidebar }) => {
         </div>
       </nav>
       <section className="w-full h-20 bg-slate-100 flex items-center justify-center">
-        <div className="w-96">
+        <div className="w-[29rem]">
           <h3 className="text-base font-medium text-gray-800 mb-1 border-blue-700">
             Search CV
           </h3>
@@ -41,13 +41,13 @@ const Header = ({ onToggleSidebar }) => {
                 />
               </svg>
             </button>
-            <div className="flex items-center space-x-2">
+            <div className="flex items-center space-x-2 w-full">
               <input
                 type="text"
                 placeholder="Experience in Hyderabad"
                 className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
-              <Button className="px-6 py-2 bg-gradient-to-r from-blue-500 to-green-500 text-white !text-white font-medium rounded-md hover:from-blue-600 hover:to-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+              <Button className="!px-5 !py-2 !font-semibold text-sm bg-gradient-to-r from-blue-500 to-green-500 !text-white rounded-md hover:from-blue-600 hover:to-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
                 Search
               </Button>
             </div>
